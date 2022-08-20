0cb6a4131d34c0af291e22df82f09f419d31099b
ThaysPrado <thaysprado96@gmail.com>
2022-08-20 01:23:00 -0300

    fix: send message and last user

--- src/main.py
+++ src/main.py
@@ -10,8 +10,6 @@ SECRET_KEY = os.getenv("TOKEN")
 last_user = ""
 count_attempt = 0
 
-channel_list = [1009650829738922056]
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -19,8 +17,10 @@ async def on_ready():
 def count_attempts(user):
     global last_user, count_attempt
     if last_user == user:
+        print(user)
         count_attempt += 1
     else:
+        print(user)
         last_user = user
         count_attempt = 0
 
@@ -34,35 +34,26 @@ def get_message_about_attempt():
     return ""
 
 def should_not_response():
-    if count_attempt == 2:
+    if count_attempt >= 2:
         return True
     
     return False
 
-def should_break(author, channel):
-    if author == client.user:
-        return True
-
-    for id in channel_list:
-        if id == channel:
-            return False
-    
-    return True
-
 @client.event
 async def on_message(message):
-    if should_break(message.author, message.channel.id):
+    if message.author == client.user:
         return
     
     if message.content.startswith('/hello'):
         await message.channel.send(service.get_introduction())
 
     if message.content.startswith('/food') or message.content.startswith('/drink'):
+        count_attempts(message.author)
+
         if should_not_response():
             await message.channel.send("Ops, suas chances acabaram. Hora de escolher! Ou tente mais tarde")
             return
 
-        count_attempts(client.user)
         about_attempts = get_message_about_attempt()
 
         if message.content.startswith('/food'):
